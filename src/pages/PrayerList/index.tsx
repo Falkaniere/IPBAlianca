4803620cfe95311b5dbcfd67244b5c9d77f3f502
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2021-04-19 23:02:07 -0300

    add screen access denied for list orders without user

--- src/pages/PrayerList/index.tsx
+++ src/pages/PrayerList/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import dayjs from 'dayjs';
-
+import { useAuth } from '../../hooks/auth';
 import CheckBox from '@react-native-community/checkbox';
 import { Container, OrderList, OrderContainer, OrderText } from './styles';
 import Header from '../../components/Header';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export interface Order {
   id: string;
@@ -15,25 +17,29 @@ export interface Order {
 
 const PrayerList: React.FC = () => {
   const [orders, setOrder] = useState<Order[]>([]);
+  const { user } = useAuth();
 
   useEffect(() => {
-    firestore()
-    .collection('orders')
-    .get()
-    .then((orders) => {
-      const arrOfOrders: any = [];
-      orders.forEach((order) => {
-        const oneOrder: Order = {
-          id: order.id,
-          reason: order.data().title,
-          isCheck: order.data().isCheck,
-          updatedAt: order.data().updatedAt,
-        }
-        arrOfOrders.push(oneOrder);
-      });
+    const getAllOrders = () => {
+      firestore()
+      .collection('orders')
+      .get()
+      .then((orders) => {
+        const arrOfOrders: any = [];
+        orders.forEach((order) => {
+          const oneOrder: Order = {
+            id: order.id,
+            reason: order.data().title,
+            isCheck: order.data().isCheck,
+            updatedAt: order.data().updatedAt,
+          }
+          arrOfOrders.push(oneOrder);
+        });
       setOrder(arrOfOrders);
-    })
+      });
+    }
 
+    getAllOrders();
     verifyIfIsAnotherDay();
   },[]);
 
@@ -59,6 +65,15 @@ const PrayerList: React.FC = () => {
       updateAt: new Date(),
     })
   };
+
+  if(!user){
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Icon name="hand-stop-o" size={70} color='#666' />
+        <Text style={{ marginTop: 15, paddingHorizontal: 5 ,fontSize: 16, color: '#666', textAlign: 'center' }}>Acesso negado, favor realizar login para acessar a página</Text>
+      </View>
+    )
+  }
 
   return (
     <Container>
